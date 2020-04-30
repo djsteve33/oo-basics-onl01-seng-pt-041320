23a0730694abebe0fe9e418e3681f48669f9aa9c
Steve Womble <swomble@aol.com>
2020-04-30 19:11:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,11 +5,11 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-end
+  end
 
-def cobble
-  self.condition = "new"
-  puts "Your shoe is as good as new!"
-end 
+  def cobble
+    self.condition = "new"
+   puts "Your shoe is as good as new!"
+  end 
 
 end
